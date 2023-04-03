81dafc0167f1a712483b313003cd108f25811b3b
TiparpronS <Sukanyat2022a@gmail.com>
2023-04-03 11:15:10 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './component/NavBar';
+
+
+
+function App() {
+  return (
+    <div className='mainBox'> 
+<NavBar/>
+
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/component/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <div className='mainNavBar'>
+        <div className='flashSale'>Get 10% off your 6 + free shipping.</div>
+        <div className='logo'>logo</div>
+        <div className='navLink'>
+<h3>Varietal</h3>
+<h3>Region</h3>
+<h3>Discover</h3>
+<h3>Gifts</h3>
+
+        </div>
+        <div className='searchBox'> SearchBox</div>
+        <div className='basket'>Basket</div>
+
+      
+    </div>
+  )
+}
+
+export default NavBar
