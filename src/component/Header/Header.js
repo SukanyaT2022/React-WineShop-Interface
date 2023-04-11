c373caf37ff5cd0132fbcc82e075831e5f58c6a0
TiparpronS <Sukanyat2022a@gmail.com>
2023-04-11 11:07:53 -0400

    add

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,51 +1,72 @@
 import './Header.css';
 import { FaWineGlassAlt } from 'react-icons/fa';
-import { MdWineBar } from 'react-icons/Md';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faWineGlass,
+  faWineGlassEmpty,
+  faChampagneGlasses,
+  faGift,
+} from '@fortawesome/free-solid-svg-icons';
+// import { MdWineBar } from 'react-icons/Md';
 
 function Header() {
   return (
     <div>
-      <div className='mainBigHero'>
-      <div className='text'>
-        <h3>All in one place</h3>
-        <hr className='line'></hr>
-        <p>This is wineShop.com country</p>
-      </div>
-   {/* button */}
+      <div className="mainBigHero">
+        <div className="text">
+          <h3>All in one place</h3>
+          <hr className="line"></hr>
+          <p>This is wineShop.com country</p>
+        </div>
+        {/* button */}
 
-      <div className='main-buttonIcon'>
+        <div className="main-buttonIcon">
+          <a href="">
+            <div className="smallButton">
+              <div className="controlIconRed">
+                {/* <FaWineGlassAlt /> */}
+                <FontAwesomeIcon icon={faWineGlass} />
+              </div>
+              <h5>Red</h5>
+            </div>
+          </a>
 
-<a href=""><div className="smallButton">
-  <div className='controlIconRed'><FaWineGlassAlt/></div>
-  <h5>Red</h5>
-</div></a>
+          <a href="">
+            <div className="smallButton">
+              <div className="controlIconWhite">
+                <FontAwesomeIcon icon={faWineGlassEmpty} />
+              </div>
+              <h5>White</h5>
+            </div>
+          </a>
 
+          <a href="">
+            <div className="smallButton">
+              <div className="controlIconSpark">
+                <FontAwesomeIcon icon={faChampagneGlasses} />
+              </div>
+              <h5>Sparkling</h5>
+            </div>
+          </a>
 
-<a href=""><div className="smallButton">
-  <div className='controlIconWhite'><MdWineBar/></div>
-  <h5>White</h5>
-</div></a>
+          <a href="">
+            <div className="smallButton">
+              <div className="controlIconRose">
+                <FaWineGlassAlt />
+              </div>
+              <h5>Rose</h5>
+            </div>
+          </a>
 
-
-<a href=""><div className="smallButton">
-  <div className='controlIconSpark'><FaWineGlassAlt/></div>
-  <h5>Sparkling</h5>
-</div></a>
-
-
-<a href=""><div className="smallButton">
-  <div className='controlIconRose'><FaWineGlassAlt/></div>
-  <h5>Rose</h5>
-</div></a>
-
-
-<a href=""><div className="smallButton">
-  <div className='controlIconGift'><FaWineGlassAlt/></div>
-  <h5>Gifts</h5>
-</div></a>
-
-
-      </div>
+          <a href="">
+            <div className="smallButton">
+              <div className="controlIconGift">
+                <FontAwesomeIcon icon={faGift} />
+              </div>
+              <h5>Gifts</h5>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
