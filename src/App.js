b6e065d7943f8a8c6ce0bdee1b37daf8c20eb876
TiparpronS <Sukanyat2022a@gmail.com>
2023-04-20 10:34:08 -0400

    add

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ import HeaderBox from './component/Header/Header';
 import Grid from './component/GridBox/Grid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './component/footer/Footer';
-
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="mainBox">
-      <NavBar />
+    <Router>
+      <div className="mainBox">
+        <NavBar />
 
-      <HeaderBox />
-      <Grid />
-      <Footer/>
-    </div>
+        <HeaderBox />
+        <Grid />
+        <Footer />
+      </div>
+
+      <Routes>
+        <Route exact path="/footer" element={<Footer />}></Route>
+        <Route exact path="/card" element={<Grid />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/component/GridBox/Grid.js
+++ src/component/GridBox/Grid.js
@@ -3,6 +3,25 @@ import './Grid.css';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import myData from './grid.json';
+// jason here
+// function wineSection1(props) {
+//   return (
+//     <div class="wholePage">
+//       <div class="section1-superHero">
+//         <h4>Super Hero Movies {props.searchMovieText}</h4>
+//         <div class="main-flexBox">
+//           {/*
+//         //below here we make pattern of data connect to json */}
+//           {myData.superHeroMovie.map((value) => {
+//             return (
+//               <div class="smallBox">
+//                 <h4>{value.section1}</h4>
+//                 <img src={value.image} />
+//                 <p>{value.Genre}</p>
+//               </div>
+//             );
+//           })}
 
 function Grid() {
   return (
@@ -10,56 +29,21 @@ function Grid() {
       {/* React Bootstrap Code */}
       <div className="container my-4">
         <Row xs={1} md={2} className="g-4">
-          {/* {Array.from({ length: 4 }).map((_, idx) => ( */}
-          <Col className="control3boxesGrid">
-            <Card>
-              <Card.Img
-                variant="top"
-                src="https://images.unsplash.com/photo-1510812431401-41d2bd2722f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGVvcGxlJTIwZHJpbmslMjB3aW5lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="control3boxesGrid">
-            <Card>
-              <Card.Img
-                variant="top"
-                src="https://images.unsplash.com/photo-1573574635896-36753f4e38bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2luZXlhcmR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-              />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="control3boxesGrid">
-            <Card>
-              <Card.Img
-                variant="top"
-                src="https://images.unsplash.com/photo-1573574635896-36753f4e38bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2luZXlhcmR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-              />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          {/* ))} */}
+          {myData.superHeroMovie.map((value, idx) => (
+            <Col className="control3boxesGrid">
+              <Card>
+                <Card.Img className="img-css" variant="top" src={value.image} />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <p>
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </div>
 
