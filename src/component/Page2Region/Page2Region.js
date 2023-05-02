3c5572c3635f698b5a0025b79df7c9f5820d0ad8
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-05-02 08:51:45 -0400

    add

--- src/component/Page2Region/Page2Region.js
+++ src/component/Page2Region/Page2Region.js
@@ -5,7 +5,35 @@ import React from 'react'
 function Page2Region() {
   return (
     <div>
-      hellooo
+
+
+<div className='bigBoxPage2'>
+  <div className='picSide-left'>
+<img src ="https://images.pexels.com/photos/12859328/pexels-photo-12859328.jpeg?auto=compress&cs=tinysrgb&w=800" />
+
+<div className='textNext2Pic'>
+<h4>Red wine from Tuscany</h4>
+<p>Region in central Italy</p>
+</div>
+  </div>
+
+
+< div className='priceSide-right'>
+<div className='priceDiscount'>
+  {/* del is cross for discount cross line for delete */}
+  <p><del>$45</del></p>
+  <p>$41<sup>99</sup></p>
+</div>
+<div className='cart'>
+<input type="number" value="1" />
+<input type="button" value='Add to Cart' />
+</div>
+
+</div>
+
+</div>
+
+
     </div>
   )
 }
