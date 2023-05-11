7074b485ac2a499624a02707ae550f9aa2f1150d
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-05-11 07:12:29 -0400

    add

--- src/component/Page3Discover/Page3Discover.js
+++ src/component/Page3Discover/Page3Discover.js
@@ -4,6 +4,7 @@ import './Page3Discover.css';
 function Page3Discover() {
   return (
     <div id="discover">
+      <h2 className='moveH2'>Wine under $25</h2>
       <div className="bigBoxPage2 my-5">
         <div className="picSide-left">
           <img src="https://images.pexels.com/photos/12859328/pexels-photo-12859328.jpeg?auto=compress&cs=tinysrgb&w=800" />
@@ -17,11 +18,9 @@ function Page3Discover() {
         <div className="priceSide-right">
           <div className="priceDiscount">
             {/* del is cross for discount cross line for delete */}
+          
             <p>
-              <del>$45</del>
-            </p>
-            <p>
-              $41<sup>99</sup>
+              $20<sup>99</sup>
             </p>
           </div>
           <div className="cart">
@@ -46,11 +45,9 @@ function Page3Discover() {
         <div className="priceSide-right">
           <div className="priceDiscount">
             {/* del is cross for discount cross line for delete */}
+          
             <p>
-              <del>$35</del>
-            </p>
-            <p>
-              $30<sup>99</sup>
+              $15<sup>99</sup>
             </p>
           </div>
           <div className="cart">
@@ -59,6 +56,61 @@ function Page3Discover() {
           </div>
         </div>
       </div>
+
+ {/* box3 */}
+
+ <div className="bigBoxPage2">
+        <div className="picSide-left">
+          <img src="https://images.unsplash.com/photo-1600492847790-80d2daa56c40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmluZXlhcmQlMjB3aW5lJTIwYXVzdHJhbGlhfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=800&q=60" />
+
+          <div className="textNext2Pic">
+            <h4>Penfolds from Australia</h4>
+            <p>Region in Western Australia</p>
+          </div>
+        </div>
+
+        <div className="priceSide-right">
+          <div className="priceDiscount">
+            {/* del is cross for discount cross line for delete */}
+          
+            <p>
+              $12<sup>99</sup>
+            </p>
+          </div>
+          <div className="cart">
+            <input type="number" value="1" />
+            <input type="button" value="Add to Cart" />
+          </div>
+        </div>
+      </div>
+
+       {/* box4 */}
+
+      <div className="bigBoxPage2">
+        <div className="picSide-left">
+          <img src="https://images.unsplash.com/photo-1600492847790-80d2daa56c40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmluZXlhcmQlMjB3aW5lJTIwYXVzdHJhbGlhfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=800&q=60" />
+
+          <div className="textNext2Pic">
+            <h4>Penfolds from Australia</h4>
+            <p>Region in Western Australia</p>
+          </div>
+        </div>
+
+        <div className="priceSide-right">
+          <div className="priceDiscount">
+            {/* del is cross for discount cross line for delete */}
+          
+            <p>
+              $18<sup>99</sup>
+            </p>
+          </div>
+          <div className="cart">
+            <input type="number" value="1" />
+            <input type="button" value="Add to Cart" />
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/component/Page4Gift/Page4Gift.js
+++ src/component/Page4Gift/Page4Gift.js
@@ -1,12 +1,40 @@
-import React from 'react'
-import './Page4Gift';
+// import React from 'react'
+// import './Page4Gift';
 
-function Page4Gift() {
+// function Page4Gift() {
+//   return (
+//     <div id="gifts">
+//       <h1>Hello0oooooooooooooooo</h1>
+//     </div>
+//   );
+// }
+
+// export default Page4Gift;
+
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+function GridExample() {
   return (
-    <div id="gifts">
-      <h1>Hello0oooooooooooooooo</h1>
-    </div>
+    <Row xs={1} md={3} className="g-4">
+      {Array.from({ length: 6 }).map((_, idx) => (
+        <Col>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   );
 }
 
-export default Page4Gift;
+export default GridExample;
