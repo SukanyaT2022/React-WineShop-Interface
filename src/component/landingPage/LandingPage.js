4254b8f5260707ab706316380912800e6c8829ee
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-05-15 08:52:59 -0400

    add

--- src/component/landingPage/LandingPage.js
+++ src/component/landingPage/LandingPage.js
@@ -2,7 +2,37 @@ import React from 'react';
 import './landingPage.css';
 
 function LandingPage() {
-  return <div>hello</div>;
+  return
+  <div className='wrapper'>
+    <div className='section1-onlytext'>
+<h5>BETTER WINE</h5>
+<h3>HOW our wines stack up to traditional retail</h3>
+    </div>
+<div className='section1-only2boxes'>
+<div className='leftBox'>
+<div className='leftBoxText'>
+
+</div>
+<div className='leftBoxImage'>
+
+</div>
+</div>
+{/* rightBox */}
+
+<div className='rightBox'>
+<div className='rightBoxImage'>
+
+</div>
+
+<div className='rightBoxText'>
+
+</div>
+</div>
+
+</div>
+    
+
+    </div>;
 }
 
 export default LandingPage;
