9432bc98ec6f63fd465dca5ec9f767003ebfc6e9
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-05-16 10:30:02 -0400

    add

--- src/component/landingPage/LandingPage.js
+++ src/component/landingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './landingPage.css';
+import { FaRegStar, FaStar } from 'react-icons/fa';
 
 function LandingPage() {
   return(
@@ -9,13 +10,31 @@ function LandingPage() {
 <h5>BETTER WINE</h5>
 <h3>HOW our wines stack up to traditional retail</h3>
     </div>
-<div className='section1-only2boxes'>
+
+<div className='section1-controlLeftRight'>
+    
 <div className='leftBox'>
 <div className='leftBoxText'>
+<p>WineShop.com</p>
+<h5>AAAAAAAaAAA Valley</h5>
+<p> AAAAAA Valley</p>
+<p>2 years in French Oak</p>
+
+<div className='controlRatingSection'>
+<p>Rating 4.0</p>
+<div className='targetOnlyStar'>
+<FaStar/>
+<FaStar/>
+<FaStar/>
+<FaStar/>
+<FaRegStar/>
+</div>
+ <p>$29.99</p>
+</div>
 
 </div>
 <div className='leftBoxImage'>
-
+<img src="/Users/tiparpron/Desktop/ReactProject/Apr3-interface/my-app/src/asset/image/wineBottle-BGremove.png"/>
 </div>
 </div>
 
@@ -31,9 +50,9 @@ function LandingPage() {
 </div>
 </div>
 
+
+
 </div>
-
-
     </div>
 )};
 
