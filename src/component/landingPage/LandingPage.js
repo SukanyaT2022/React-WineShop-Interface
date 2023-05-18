60e48f1b103bfb1e3163846350f07f4b8dd3779a
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-05-18 07:11:29 -0400

    add

--- src/component/landingPage/LandingPage.js
+++ src/component/landingPage/LandingPage.js
@@ -6,6 +6,27 @@ function LandingPage() {
   return(
   
   <div className='wrapper'>
+{/* start welcome section */}
+<div className='mainbox-welcome'>
+    <div className='imageBox-welcome'>
+      
+<img src="https://images.unsplash.com/photo-1667227254248-b4169ead3c64?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmluZXlhcmRzJTIwaGFydmVzdCUyMGZhcm1lcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" alt='farmerPiconTop'/>
+    </div>
+    <div className='rightBox-welcome'>
+<h4>Welcome to wineShop.com</h4>
+<h3>Support independent winemakers.<br/> Get better wine in return</h3>
+  <p>With the support of 300,000 wine drinkers, we fund talented, independent winemakers up front. This way, they get to spend all their attention on making you the best wine possible.</p>
+
+  <a><input type="button" value = "Get Started"/></a>
+  
+  
+    </div>
+
+</div>
+
+{/* end welcome section */}
+
+
     <div className='section1-onlytext'>
 <h5>BETTER WINE</h5>
 <h3>HOW our wines stack up to traditional retail</h3>
@@ -39,7 +60,6 @@ function LandingPage() {
 
 </div>
 </div>
-
 
 
 <div className='rightBox'>
